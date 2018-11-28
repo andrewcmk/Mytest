d00cf4a9596d6c7356a4fa9ed8d792885c2622bd
andrewcmk <andrewcmk97@gmail.com>
2018-11-29 02:16:13 +0800

    some_message

---
+++ sample.py
@@ -0,0 +1,16 @@
+print("Hello Github Universe!")
+
+'''
+This is my vimrc conf
+  1 syntax on                                                         
+  2 set expandtab                                                     
+  3 set tabstop=4                                                     
+  4 set shiftwidth=4                                                  
+  5 set expandtab                                                     
+  6 set ai                                                            
+  7 set number                                                        
+  8 set hlsearch                                                      
+  9 set ruler                                                         
+ 10 highlight Comment ctermfg=green                                   
+'''                                 
+
